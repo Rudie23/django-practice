133e8a3001fbd83061351e98be7a943989b621ef
Diego <ruan.diego.anjos@outlook.com>
2023-04-30 16:44:56 -0300

    Layout

--- djangopro/base/views.py
+++ djangopro/base/views.py
@@ -1,9 +1,8 @@
-from django.http import HttpResponse
+
 from django.shortcuts import render
 
 # Create your views here.
-app_name = 'base'
 
 
 def home(request):
-    return HttpResponse("Hello World")
+    return render(request, 'base/home.html')
