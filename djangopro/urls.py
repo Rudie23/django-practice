af25a847bed1c571d44945f5002e8e16454215c2
Diego <ruan.diego.anjos@outlook.com>
2023-04-29 21:38:24 -0300

    Sentry

--- djangopro/urls.py
+++ djangopro/urls.py
@@ -14,12 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 from djangopro.base.views import home
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path('', home),
 ]
+
+if settings.DEBUG:
+    urlpatterns.append(
+        path('__debug__/', include('debug_toolbar.urls')),
+    )
