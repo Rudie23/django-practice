6452d3ecf09bed07f0cb5df7ea905bb9054c9774
Diego <ruan.diego.anjos@outlook.com>
2023-05-05 16:26:39 -0300

    model_bakery

--- djangopro/videos/tests/test_indice.py
+++ djangopro/videos/tests/test_indice.py
@@ -1,11 +1,18 @@
+from model_bakery import baker
 import pytest
 from django.urls import reverse
 from djangopro.django_assertions import assert_contains
+from djangopro.videos.models import Video
+
+
+@pytest.fixture
+def videos(db):
+    return baker.make(Video, 3)
 
 
 @pytest.fixture
 def resp(client, videos):
-    return client.get(reverse('videos:indice'))
+    return client.get(reverse('videos:index'))
 
 
 def test_status_code(resp):
