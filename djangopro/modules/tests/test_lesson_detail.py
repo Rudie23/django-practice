91f9ecae87c08c5772dcdcbb0971a74a72d7f3c8
Rudie23 <ruan.diego.anjos@outlook.com>
2023-06-14 23:04:05 -0300

    Page Lesson created

---
+++ djangopro/modules/tests/test_lesson_detail.py
@@ -0,0 +1,26 @@
+import pytest
+from django.urls import reverse
+from djangopro.django_assertions import assert_contains
+from model_bakery import baker
+
+from djangopro.modules.models import Module, Lesson
+
+
+@pytest.fixture
+def module(db):
+    return baker.make(Module)
+
+
+@pytest.fixture
+def lesson(module):
+    return baker.make(Lesson, module=module)
+
+
+@pytest.fixture
+def resp(client, lesson):
+    resp = client.get(reverse('modules:lesson', kwargs={'slug': lesson.slug}))
+    return resp
+
+
+def test_lesson_title(resp, lesson: Lesson):
+    assert_contains(resp, lesson.title)
