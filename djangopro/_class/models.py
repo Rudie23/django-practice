740413f914dc148a465213b9d22d3801e01b3b40
Rudie23 <ruan.diego.anjos@outlook.com>
2023-06-26 19:07:06 -0300

    Enrollment e Class models have created.

--- djangopro/_class/models.py
+++ djangopro/_class/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -8,6 +9,21 @@ class Class(models.Model):
     slug = models.SlugField(max_length=64)
     begin = models.DateField()
     end = models.DateField()
+    students = models.ManyToManyField(get_user_model(), through='Enrollment')
 
     def __str__(self):
         return self.name
+
+
+class Enrollment(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    _class = models.ForeignKey(Class, on_delete=models.CASCADE)
+
+    # With class Meta, you can restrict a creation of enrollment to once time
+    class Meta:
+        unique_together = [['user', '_class']]
+        ordering = ['_class', 'date']
+
+    def __str__(self):
+        return self.user
