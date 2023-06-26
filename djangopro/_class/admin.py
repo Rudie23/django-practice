80e404bd9483cd2394256d8a57ea0f8c9b35cb9a
Rudie23 <ruan.diego.anjos@outlook.com>
2023-06-26 19:07:06 -0300

    Enrollment e Class models have created.

--- djangopro/_class/admin.py
+++ djangopro/_class/admin.py
@@ -4,9 +4,18 @@ from djangopro._class.models import Class
 
 
 # Register your models here.
+# admin.TabularInLine or admin.StackedInline
+class EnrollmentInLine(admin.StackedInline):
+    model = Class.students.through
+    extra = 1
+    readonly_fields = ('date',)
+    autocomplete_fields = ('user',)
+    ordering = ('-date',)
+
 
 @admin.register(Class)
 class ClassAdmin(admin.ModelAdmin):
+    inlines = [EnrollmentInLine]
     list_display = ('name', 'slug', 'begin', 'end',)
     prepopulated_fields = {'slug': ('name',)}
     ordering = ('-begin',)
