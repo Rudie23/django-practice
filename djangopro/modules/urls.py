80044e76fdeb3d3aefba2079566ab3a23dc0ca38
Rudie23 <ruan.diego.anjos@outlook.com>
2023-06-26 15:55:17 -0300

    Page index created.

--- djangopro/modules/urls.py
+++ djangopro/modules/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
-from djangopro.modules.views import detail, lesson
+from djangopro.modules.views import detail, lesson, index
 
 app_name = 'modules'
 
 urlpatterns = [
     path('<slug:slug>', detail, name='detail'),
     path('lesson/<slug:slug>', lesson, name='lesson'),
+    path('', index, name='index')
 
 ]
