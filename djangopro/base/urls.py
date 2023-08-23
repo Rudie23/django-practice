26c5df4ad7e942005bd353f53b6f064f46b67265
Diego <ruan.diego.anjos@outlook.com>
2023-08-22 23:58:33 -0300

    Tests login and home working completely

--- djangopro/base/urls.py
+++ djangopro/base/urls.py
@@ -1,9 +1,11 @@
 
 from django.urls import path
 from djangopro.base.views import home
+from djangopro.base import views
 
 app_name = 'base'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('accounts/login', views.login_user, name='login')
 ]
--- djangopro/base/views.py
+++ djangopro/base/views.py
@@ -1,5 +1,6 @@
-
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+from django.shortcuts import render, redirect
 
 
 # Create your views here.
@@ -7,3 +8,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html', {})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        email = request.POST["email"]
+        password = request.POST["password"]
+        user = authenticate(request, email=email, password=password)
+        if user is not None:
+            # Redirect to a success page.
+            login(request, user)
+            return redirect('modules:index')
+        else:
+            messages.success(request, "There was an error logging in, try again...")
+            # Return an 'invalid login' error message.
+            return redirect('login')
+    return render(request, 'registration/login.html', {})
